94157b611738bc766990eaa4f06c437bc8d78fdf
evo264714 <mh264714@gmail.com>
2023-02-20 19:26:19 +0600

    done

---
+++ js/problems.js
@@ -0,0 +1,24 @@
+//* 1-->
+// const multiply = (num1, num2, num3) => {
+//     const result = num1 * num2 * num3;
+//     return result;
+// }
+
+// const total = multiply(2, 2, 2);
+// console.log(total);
+
+//* 2-->
+const lines = `
+I am a web developer
+I love to code
+I love to eat
+`
+// console.log(lines);
+
+//* 3-->
+const add = (num1, num2 = 0) => {
+    const total = num1 + num2;
+    return total;
+}
+const result = add(20);
+console.log(result);
